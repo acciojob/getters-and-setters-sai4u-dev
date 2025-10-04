a177584ea0723a90ba0954f36ab164164016468c
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-04 22:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-//complete this code
 class Person {
 	constructor(name, age) {
-		this.name = name;
-		this.age = age;
+		this._name = name; // private backing fields
+		this._age = age;
 	}
 
+	// Getter for name
 	get name() {
-		return this.name;
+		return this._name;
 	}
 
-	set age() {
-		return this.age;
+	// Setter for name
+	set name(newName) {
+		this._name = newName;
+	}
+
+	// Getter for age
+	get age() {
+		return this._age;
+	}
+
+	// Setter for age
+	set age(newAge) {
+		this._age = newAge;
 	}
 }
 
 class Student extends Person {
 	study() {
-		console.log(`${this.name} is studting`);
+		console.log(`${this.name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach() {
-		console.log(`${this.name} is teaching`)
+		console.log(`${this.name} is teaching`);
 	}
 }
 
